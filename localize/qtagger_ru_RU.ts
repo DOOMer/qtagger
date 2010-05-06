91de7c905fddc5ef44d53170a8ae6bbd4cf49b9e
DOOMer <doomer3d@gmail.com>
2010-05-06 17:47:57 +0400

    add Russian translation

---
+++ localize/qtagger_ru_RU.ts
@@ -0,0 +1,328 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="../src/configdialog.ui" line="29"/>
+        <source>Setting - qTagger</source>
+        <translation>Настройки - qTagger</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.ui" line="47"/>
+        <source>Encoding</source>
+        <translation>Кодировка</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.ui" line="55"/>
+        <source>Source text encoding:</source>
+        <translation>Исходная кодировка текста:</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.ui" line="77"/>
+        <source>Toolbar</source>
+        <translation>Панель инструментов</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.ui" line="85"/>
+        <source>Text and icon</source>
+        <translation>Текст и значки</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.ui" line="92"/>
+        <source>Only icon</source>
+        <translation>Тоько значки</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.ui" line="99"/>
+        <source>Only text</source>
+        <translation>Только текст</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.ui" line="129"/>
+        <source>Defaults</source>
+        <translation>По умолчанию</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.ui" line="152"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.ui" line="166"/>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="28"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="166"/>
+        <source>Message</source>
+        <translation type="unfinished">Сообщение</translation>
+    </message>
+    <message>
+        <location filename="../src/configdialog.cpp" line="166"/>
+        <source>Settings will be restored to default values!</source>
+        <translation>Настройки приведены в исх-одные значения!</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="26"/>
+        <source>qTagger</source>
+        <translation>qTagger</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="169"/>
+        <source>Select All</source>
+        <translation>Выбрать все</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="198"/>
+        <source>Title:</source>
+        <translation>Название:</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="237"/>
+        <source>Artist:</source>
+        <translation>Исполнитель:</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="276"/>
+        <source>Album:</source>
+        <translation>Альбом:</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="346"/>
+        <source>Genre:</source>
+        <translation>Жанр:</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="372"/>
+        <source>Year:</source>
+        <translation>Год:</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="418"/>
+        <source>Track #:</source>
+        <translation type="unfinished">Трек №:</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="450"/>
+        <source>Comment:</source>
+        <translation>Комментарий:</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="515"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="535"/>
+        <source>Cancel</source>
+        <translation type="unfinished">Отмена</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="26"/>
+        <source>Select all</source>
+        <translation>Выбрать все</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="26"/>
+        <source>Select none</source>
+        <translation type="unfinished">Отменить выбор</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="55"/>
+        <source>Add Files</source>
+        <translation>Добавить файл</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="58"/>
+        <source>Add Dir</source>
+        <translation>Добавить каталог</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="61"/>
+        <source>To Unicode</source>
+        <translation>Перекодировка</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="64"/>
+        <source>Remover</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="67"/>
+        <source>Clear</source>
+        <translation>Очистить</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="70"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="73"/>
+        <location filename="../src/mainwindow.cpp" line="100"/>
+        <source>Help</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="76"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="79"/>
+        <source>About Qt</source>
+        <translation>О Qt</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="82"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="99"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="182"/>
+        <source>Add File</source>
+        <translation>Добавить файл</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="318"/>
+        <source>Help file missing in: </source>
+        <translation>Файл справки отсутствует в: </translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="195"/>
+        <source>Add directory</source>
+        <translation>Добавить каталог</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="315"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="317"/>
+        <source>Help file not found</source>
+        <translation>Файл справки не найден</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="465"/>
+        <source>Bitrate: </source>
+        <translation type="unfinished">Битрейт: </translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="466"/>
+        <source>Sample rate: </source>
+        <translation>Дискретизация: </translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="467"/>
+        <source>Time: </source>
+        <translation type="unfinished">Время: </translation>
+    </message>
+</context>
+<context>
+    <name>TrackModel</name>
+    <message>
+        <location filename="../src/trackmodel.cpp" line="52"/>
+        <source>Title</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="../src/trackmodel.cpp" line="54"/>
+        <source>Artist</source>
+        <translation>Исполнитель</translation>
+    </message>
+    <message>
+        <location filename="../src/trackmodel.cpp" line="56"/>
+        <source>Album</source>
+        <translation>Альбом</translation>
+    </message>
+    <message>
+        <location filename="../src/trackmodel.cpp" line="58"/>
+        <source>Time</source>
+        <translation type="unfinished">Время</translation>
+    </message>
+</context>
+<context>
+    <name>aboutdialog</name>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="32"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="107"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Serif&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;table border=&quot;0&quot; style=&quot;-qt-table-type: root; margin-top:4px; margin-bottom:4px; margin-left:4px; margin-right:4px;&quot;&gt;
+&lt;tr&gt;
+&lt;td style=&quot;border: none;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:14pt; font-weight:600;&quot;&gt;qTagger&lt;/span&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="124"/>
+        <source>Simple ID3 tag editor</source>
+        <translation>Простой редактор ID3-тегов</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="134"/>
+        <source>Build in </source>
+        <translation>Собран </translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="147"/>
+        <source>Using Qt </source>
+        <translation>использует </translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="170"/>
+        <source>&amp;copy; 2010, Artem Galichkin</source>
+        <translation>&amp;copy; 2010, Артём Галичкин</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="186"/>
+        <source>Licensed by GPL v2</source>
+        <translation>Лицензировано по GPL v2</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="196"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Serif&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;table style=&quot;-qt-table-type: root; margin-top:4px; margin-bottom:4px; margin-left:4px; margin-right:4px;&quot;&gt;
+&lt;tr&gt;
+&lt;td style=&quot;border: none;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;http://qtagger.googlecode.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;Home Page&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Serif&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;table style=&quot;-qt-table-type: root; margin-top:4px; margin-bottom:4px; margin-left:4px; margin-right:4px;&quot;&gt;
+&lt;tr&gt;
+&lt;td style=&quot;border: none;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;a href=&quot;http://qtagger.googlecode.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0057ae;&quot;&gt;Веб-сайт&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.ui" line="253"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+</context>
+</TS>
